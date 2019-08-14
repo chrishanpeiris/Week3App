7e7899fd0d8a665d30544e54a9bdf171b901cb4b
chrishanpeiris <chrishanmx@gmail.com>
2019-08-14 09:39:17 +0930

    Added mvc project

---
+++ Week3App/Week3App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Week3App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
